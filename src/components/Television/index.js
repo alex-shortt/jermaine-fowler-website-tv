f62e91ab99e600e66e578d616023949c6d139d4d
Alex Shortt <alexander.shortt@gmail.com>
2019-09-03 19:22:58 +0200

    add television svg and position content

--- src/components/Television/index.js
+++ src/components/Television/index.js
@@ -1,5 +1,30 @@
 import React from "react"
+import styled from "styled-components/macro"
+
+import Monitor from "assets/television/Monitor"
+
+const Container = styled.div`
+  position: relative;
+  width: 48rem;
+  height: 48rem;
+`
+
+const Display = styled.div`
+  position: absolute;
+  width: 90%;
+  left: 5%;
+  height: 70%;
+  top: 9%;
+  background: red;
+  opacity: 0.7;
+  z-index: -1;
+`
 
 export default function Television(props) {
-  return <>tv</>
+  return (
+    <Container>
+      <Monitor />
+      <Display />
+    </Container>
+  )
 }
