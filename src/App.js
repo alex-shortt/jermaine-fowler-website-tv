58c4bd34caf0b56f397cf41e8d31ec855ac3ca0d
Alex Shortt <alexander.shortt@gmail.com>
2019-10-20 17:40:07 -0700

    remove siteuserview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useEffect } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import useReactRouter from "use-react-router"
+import styled from "styled-components/macro"
 
 import GlobalStyles from "styles/globalStyles"
 import FullScreenLoading from "components/FullScreenLoading"
 import ScrollToTop from "components/ScrollToTop"
 import GA from "services/ga"
 
-const SiteUserView = React.lazy(() => import("scenes/SiteUserView"))
+const Viewport = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+const View = React.lazy(() => import("scenes/View"))
 
 const GoogleAnalytics = () => {
   const { location } = useReactRouter()
@@ -22,12 +31,14 @@ export default function App() {
       <React.Suspense fallback={<FullScreenLoading />}>
         <Router>
           <GoogleAnalytics />
-          <ScrollToTop>
-            <Switch>
-              <Route path="/" component={SiteUserView} />
-              {/* TODO: 404 Page */}
-            </Switch>
-          </ScrollToTop>
+          <Viewport>
+            <ScrollToTop>
+              <Switch>
+                <Route path={["/:page", "/"]} component={View} />
+                {/* TODO: 404 Page */}
+              </Switch>
+            </ScrollToTop>
+          </Viewport>
         </Router>
       </React.Suspense>
     </>
